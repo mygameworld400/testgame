1e2b445e32041b038a964c4af14971b3fb3af0e6
mygameworld400 <mymymy201128@gmail.com>
2026-08-25 01:48:01 +0900

    Update CloudCandyTown.jsx

--- src/CloudCandyTown.jsx
+++ src/CloudCandyTown.jsx
@@ -2113,11 +2113,6 @@ function Town({ me, setMe, onKick }) {
             <div className="ccRoomLayer">
               {scene === "movie" && movie && (
                 <div className="ccScreenWrap">
-                     style={{
-                       width: 520,
-                       height: 300,
-                     }}
-                >
                   <video
                     ref={vidRef}
                     className="ccScreenVid"
